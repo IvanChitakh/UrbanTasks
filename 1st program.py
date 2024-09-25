29c25e008e29bb7990ea548a34bdc656d104f7c0
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-09-25 15:45:11 +0300

    First Task

---
+++ 1st program.py
@@ -0,0 +1,19 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+print('\n')
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('\n')
+
+# "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+print('\n')
+
+# "4th program"
+NumberStr = '123.456'
+NumberInt = float(NumberStr)
+FirstAfterDot = NumberInt * 10 % 10
+print(int(FirstAfterDot))

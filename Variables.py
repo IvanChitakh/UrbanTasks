1664b615cf8a9a32b3283579989800155ed161f6
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-09-30 00:58:45 +0300

    Variables

---
+++ Variables.py
@@ -0,0 +1,6 @@
+completed_H_W = 12
+used_Hours = 1.5
+course_Name = 'Python'
+time_For_One_Task = used_Hours / completed_H_W
+
+print(f'Курс: {course_Name}, всего задач: {completed_H_W}, затрачено часов: {used_Hours}, среднее время выполнения: {time_For_One_Task} часа.')

7619576914d9cc900ffed6e939a0be454b4cb54d
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-10-05 05:28:24 +0300

    Create For.py

---
+++ For.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+number = 2
+flag = True
+
+for item in numbers:
+    if item == 1:
+        continue
+    while number < item:
+        if item % number == 0:
+            not_primes.append(item)
+            flag = False
+            break
+        number += 1
+    if flag == True:
+        primes.append(item)
+    number = 2
+    flag = True
+         
+print(f'Primes: {primes}')
+print(f'Not Primes: {not_primes}')

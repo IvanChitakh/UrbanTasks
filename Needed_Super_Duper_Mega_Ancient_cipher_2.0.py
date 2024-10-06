362f9330d04add684e6ba87be505dc6dd7dcd415
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-10-06 23:09:48 +0300

    Create Needed_Super_Duper_Mega_Ancient_cipher_2.0.py

---
+++ Needed_Super_Duper_Mega_Ancient_cipher_2.0.py
@@ -0,0 +1,22 @@
+# The needed solution
+def cipher(stone1):
+    dividers = []
+    right_Cipher = ''
+    for i in range(3, stone1 + 1):
+        if stone1 % i == 0:
+            dividers.append(i)
+    print(dividers)
+    iter = 1
+    while iter <= int(stone1 / 2):
+        for divider in dividers:
+            if divider / iter <= 2:
+                continue
+            print(str(iter), str(divider - iter))
+            right_Cipher += str(iter) + str(divider - iter)
+        iter += 1
+
+    return right_Cipher
+
+
+stone1 = int(input())
+print(cipher(stone1))

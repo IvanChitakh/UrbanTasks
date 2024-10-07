48e827fd8dc0fc9c89efb550fdc1918280ce6b2a
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-10-08 00:15:31 +0300

    Update Get_Matrix.py

--- Get_Matrix.py
+++ Get_Matrix.py
@@ -1,6 +1,13 @@
 def get_matrix(size_of, amount, value):
-    matrix1 = [[value] * amount] * size_of
-    return matrix1
+    if size_of <= 0:
+        return []
+    elif amount <= 0:
+        return []
+    elif value <= 0:
+        return []
+    else:
+        matrix = [[value] * amount] * size_of
+        return matrix
 
 
 matrix1 = get_matrix(2, 2, 10)
@@ -9,9 +16,11 @@ matrix3 = get_matrix(4, 2, 13)
 matrix4 = get_matrix(10, -1, 100)
 matrix5 = get_matrix(-1, 10, 100)
 matrix6 = get_matrix(-1000, -1000, 1000)
+matrix7 = get_matrix(0, 0, 1000)
 print(matrix1)
 print(matrix2)
 print(matrix3)
 print(matrix4)
 print(matrix5)
 print(matrix6)
+print(matrix7)

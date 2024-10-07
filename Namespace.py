968551ac37dee70247b6bcd33b9680f66e48cb47
IvanChitakh <137526627+IvanChitakh@users.noreply.github.com>
2024-10-08 00:42:13 +0300

    Update Namespace.py

--- Namespace.py
+++ Namespace.py
@@ -1,15 +1,19 @@
-counter = 0
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
 
 
 def string_info(word):
-    global counter
-    counter += 1
+    count_calls()
     return len(word), word.upper(), word.lower()
 
 
 def is_contains(word, list):
-    global counter
-    counter += 1
+    count_calls()
     list1 = ' '.join(list).lower()
     list1 = list1.split(' ')
     word = word.lower()
@@ -19,12 +23,8 @@ def is_contains(word, list):
         return False
 
 
-def calls():
-    return counter
-
-
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-print(calls())
+print(calls)
